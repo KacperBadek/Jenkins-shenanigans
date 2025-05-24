e84485dceef55df8d51fd2eb901d9be2c3a93df1
KacperBadek <k.badek2001@gmail.com>
2025-05-24 17:33:00 +0200

    First commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    void testHello() {
+        assertEquals(2, 1 + 1);
+    }
+}
